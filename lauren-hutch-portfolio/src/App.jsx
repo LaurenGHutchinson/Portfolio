1b867b782649788677b98a68102f1f96358f637c
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-10-04 15:45:50 -0700

    Imported components into the main app

--- lauren-hutch-portfolio/src/App.jsx
+++ lauren-hutch-portfolio/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
 
 function App() {
 
