5f221a286513df8c73dfbb38e95342aa9d4b12d0
Lauren Hutchinson <lauren.adqueen@gmail.com>
2024-10-04 15:42:45 -0700

    Commiting initial folder structure of web app

---
+++ lauren-hutch-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.scss'
+
+
+
+function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
---
+++ lauren-hutch-portfolio/src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Projects.scss'
+
+
+function Projects() {
+  return (
+    <div>Projects</div>
+  )
+}
+
+export default Projects
